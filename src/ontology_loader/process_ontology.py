e1eed92d9b88aa7a87adb9de95a99f8ff2beccba
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-02-07 16:47:55 -0800

    add oaklib and sqlite file processor

--- src/ontology_loader/process_ontology.py
+++ src/ontology_loader/process_ontology.py
@@ -6,7 +6,14 @@ def process_ontology(ontology: str):
     adapter.precompute_lookups()
     for entity in adapter.entities():
         if entity.startswith("ENVO:"):
-            print(list(adapter.ancestors(entity, reflexive=True, predicates=["rdfs:subClassOf", "BFO:0000050"])))
+            # Convert generator to list
+            ancestors_list = list(adapter.ancestors(entity, reflexive=True, predicates=["rdfs:subClassOf", "BFO:0000050"]))
+
+            # Only print if the list contains terms that start with "ENVO:"
+            filtered_ancestors = [a for a in ancestors_list if a.startswith("ENVO:")]
+
+            if filtered_ancestors:  # Print only if the list is non-empty
+                print(f"{entity} -> {filtered_ancestors}")
 
 if __name__ == "__main__":
     process_ontology("envo")
