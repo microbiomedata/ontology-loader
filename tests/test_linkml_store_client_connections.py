a2c282c2fd81d21fa1192e6122077eb1f5bda942
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-02-13 17:32:52 -0800

    reformat

---
+++ tests/test_linkml_store_client_connections.py
@@ -0,0 +1,51 @@
+"""Test MongoDB client connections from pymongo and linkml-store."""
+
+import os
+
+from linkml_store.api.client import Client
+from pymongo import MongoClient
+
+# MongoDB Connection Parameters
+MONGO_HOST = "localhost"
+MONGO_PORT = 27018
+MONGO_DB = "nmdc"  # Database where you want to insert/update
+MONGO_USER = "admin"
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "")
+AUTH_DB = "admin"  # Authentication database
+
+
+def test_mongo_client():
+    """Test MongoDB client connection from pymongo."""
+    # Initialize the MongoDB client
+    client = MongoClient(
+        host=f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={AUTH_DB}"
+    )
+
+    # Select the target database for insert/update operations
+    db = client[MONGO_DB]
+
+    print("Connected to MongoDB:", db)
+    # Verify connection (List collections)
+    print("Collections in database:", db.list_collection_names())
+
+    # Example Insert
+    collection = db["ontology_class_set"]
+    collection.insert_one({"id": "test_id", "name": "Test Ontology Class"})
+
+    # Fetch inserted data
+    result = collection.find_one({"id": "test_id"})
+    print("Inserted Record:", result)
+
+
+def test_linkmlstore_client():
+    """Test the MongoDB client connection from linkml-store."""
+    client = Client(
+        handle=f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={AUTH_DB}"
+    )
+    db = client.attach_database(
+        handle=f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={AUTH_DB}",
+        alias=MONGO_DB,
+    )
+    print("Connected to MongoDB:", db.metadata)
+    # Verify connection (List collections)
+    print("Collections in database:", db.list_collection_names())
